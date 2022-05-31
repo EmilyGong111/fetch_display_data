dff7ba7311f59ab1797b81aa0d42a2ccd88efdf5
Emily <mei.gong@icloud.com>
2022-05-31 13:33:39 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString())
+  setInterval(()=>setTime(new Date().toLocaleTimeString()), 1000)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    {time}
     </div>
   );
 }
